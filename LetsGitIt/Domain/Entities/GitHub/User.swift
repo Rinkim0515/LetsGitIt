375edc33d7c14906bd54e282c4fe70ca532047c9
Rinkim0515 <kimrindev@gmail.com>
2025-06-02 01:13:08 +0900

    폴더링

---
+++ LetsGitIt/Domain/Entities/GitHub/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  LetsGitIt
+//
+//  Created by KimRin on 6/2/25.
+//
+
+import Foundation
+
+struct User {
+    let id: Int
+    let login: String
+    let name: String?
+    let avatarURL: String
+    let bio: String?
+    let publicRepos: Int
+    let followers: Int
+    let following: Int
+}
